020c798f1973e9ed37643ab564fbc6000e522ec4
Arom2007 <arompradhan@gmail.com>
2026-02-10 13:38:29 +0545

    added methods to create initial data (when app opened for first time ever), load data and update data

--- lib/data/database.dart
+++ lib/data/database.dart
@@ -6,6 +6,25 @@ class ToDoDatabase {
   final _myBox = Hive.box("myBox");
 
 
+  // run this method if this is the first time ever opening this app
+  void createInitialData() {
+    todoList = [
+      ["Add a task", false],
+      ["Complete all tasks", false]
+    ];
+  }
 
+
+  // load the data from the database
+  void loadData() {
+    todoList = _myBox.get("TODOLIST");
+  }
+
+
+
+  // update the database
+void updateDataBase() {
+  _myBox.put("TODOLIST", todoList);
+}
 
 }
